6c16bb875e8520a76582acbf5d46a124c80cb31b
Shobhit <shobhittrivedi1111@gmail.com>
2024-04-11 17:13:00 +0530

    Initial commit

---
+++ src/test/java/com/nagarro/productCommunity/CommunityServer/CommunityServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nagarro.productCommunity.CommunityServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
